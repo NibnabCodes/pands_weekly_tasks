b7b8a70ac34a7ee3e177be4696b25d5a235d19b9
Niamh Hogan <hniamhh1995@gmail.com>
2025-03-11 13:32:12 +0000

    task6

---
+++ week06Task/squareroot.py
@@ -0,0 +1,10 @@
+# squareroot.py
+# This program creates a function that takes a positive floating-point number (float) as input and outputs an approximation of its square root utilizing the Newton method.
+# author: Niamh Hogan
+
+# Set the input as float
+num = float(input("Please enter a positive floating point number: "))
+
+# Define the function to calculate the square root
+def sqrt(num):
+    
