45d6bc09cca1cb25f6ff4f33a03593dbd100df5d
Niamh Hogan <hniamhh1995@gmail.com>
2025-03-13 17:39:22 +0000

    imported text file for task7

--- week07Task/es.py
+++ week07Task/es.py
@@ -1,5 +1,6 @@
 # es.py
 # A program that reads in a text file and outputs the number of e's it contains.
 # The program takes the filename from an argument on the command line.
+# Extra: The program also deals with errors eg no argument, filename that does not exist, or is not a text file.
 # Author: Niamh Hogan
 
