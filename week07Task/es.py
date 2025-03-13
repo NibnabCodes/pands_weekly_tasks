7bf980015c7843ed8d2f0e1cedb8165e797feff9
Niamh Hogan <hniamhh1995@gmail.com>
2025-03-13 17:14:36 +0000

    week7

---
+++ week07Task/es.py
@@ -0,0 +1,5 @@
+# es.py
+# A program that reads in a text file and outputs the number of e's it contains.
+# The program takes the filename from an argument on the command line.
+# Author: Niamh Hogan
+
