8628cdfa76a96d8863b7b578699709400b446573
Niamh Hogan <hniamhh1995@gmail.com>
2025-03-23 14:39:33 +0000

    Task8_SetUp

---
+++ week08Task/plottask.py
@@ -0,0 +1,7 @@
+# plottask.py
+# This program creates & displays a histogram of a normal distribution with 1000 values that have a mean of 5 and standard deviation of 2, 
+# and a plot of the function, h(x)=x3, in the range 0 - 10, on the one set of axes. 
+# Author: Niamh Hogan
+
+import numpy as np
+import matplotlib.pyplot as plt
